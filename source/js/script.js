746258b70493004f8cae37e14ce5de9359700658
Сергей <Lye4@mail.ru>
2023-02-15 22:22:35 +0300

    добавляет открытие меню шапки

--- source/js/script.js
+++ source/js/script.js
@@ -1,1 +1,15 @@
 /* в этот файл добавляет скрипты*/
+let navMenu = document.querySelector('.nav-menu');
+let navToggle = document.querySelector('.nav-menu__toggle');
+
+navMenu.classList.remove('nav-menu--nojs');
+
+navToggle.addEventListener('click', function () {
+  if (navMenu.classList.contains('nav-menu--closed')) {
+    navMenu.classList.remove('nav-menu--closed');
+    navMenu.classList.add('nav-menu--opened');
+  } else {
+    navMenu.classList.remove('nav-menu--opened');
+    navMenu.classList.add('nav-menu--closed');
+  }
+});
